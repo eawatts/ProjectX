1afb91b08631b9852154f026094a31627b1d721a
eawatts <ewattsie@hotmail.com>
2016-09-28 14:30:53 +0000

    Added all annotations to review entity

--- projectx/src/main/java/projectx/assets/Review.java
+++ projectx/src/main/java/projectx/assets/Review.java
@@ -1,18 +1,44 @@
 package projectx.assets;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "Review")
 public class Review {
-
 		
+		@Id
+		@Column(name = "id", nullable = false)
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
+		
+		@Column(name = "rating", nullable = false)
+		@NotNull
 		private Long rating;
-		private String comment;
+		
+		@Column(name = "comment", length = 512)
+		@Size (min = 2, max = 512)
+		private String comment;	
+		
+		@ManyToOne
+		@JoinColumn(name = "user_id", nullable = false)
+		@NotNull
 		private User reviewer;
+		
+		@ManyToOne
+		@JoinColumn(name = "product_id", nullable = false)
+		@NotNull
 		private Product product;
-		
-		
 		
 		public Long getId() {
 			return id;
@@ -36,4 +62,8 @@ public class Review {
 			return product;
 		}
 		
+		//TODO Named Queries
+//		@NamedQueries ({
+//			@NamedQuery(name = "", query = "")
+//		})
 }
