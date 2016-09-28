214dc9dd54c29f2e4afdb69799871a6f9e53599c
ThomasAnkcorn <thomasankcorn@gmail.com>
2016-09-28 13:30:34 +0100

    Address and OrderType are partially complete

--- projectx/src/main/java/projectx/assets/Address.java
+++ projectx/src/main/java/projectx/assets/Address.java
@@ -1,5 +1,98 @@
 package projectx.assets;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Address")
 public class Address {
+
+	@Id
+	@Column(name = "id", nullable = false)
+	private int id;
+
+	@Column(name = "line_1")
+	private String line_1;
+	
+	@Column(name = "line_2")
+	private String line_2;
+	
+	@Column(name = "postcode")
+	private String postcode;
+
+	/**
+	 * Constructor
+	 * 
+	 * @author Tom ;)
+	 * @param id
+	 * @param line_1
+	 * @param line_2
+	 * @param postcode
+	 * 
+	 */
+	Address(int id, String line_1, String line_2, String postcode) {
+		this.id = id;
+		this.line_1 = line_1;
+		this.line_2 = line_2;
+		this.postcode = postcode;
+	}
+
+	// GETTERS
+
+	/**
+	 * getID method
+	 * 
+	 * @return Address id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * getLine_1
+	 * 
+	 * @return Address line_1
+	 */
+	public String getLine_1() {
+		return line_1;
+	}
+
+	/**
+	 * getLine_2
+	 * 
+	 * @return Address line_2
+	 */
+	public String getLine_2() {
+		return line_2;
+	}
+
+	/**
+	 * getPostcode
+	 * 
+	 * @return Address postcode
+	 */
+	public String getPostcode() {
+		return postcode;
+	}
+
+	// SETTERS
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setLine_1(String line_1) {
+		this.line_1 = line_1;
+	}
+
+	public void setLine_2(String line_2) {
+		this.line_2 = line_2;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
 
 }
--- projectx/src/main/java/projectx/assets/OrderType.java
+++ projectx/src/main/java/projectx/assets/OrderType.java
@@ -1,5 +1,59 @@
 package projectx.assets;
 
-public enum OrderType {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "OrderType")
+public class OrderType {
+	@Id
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 * @param name
+	 */
+
+	OrderType(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * 
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
