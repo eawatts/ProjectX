c8d8e86de0c7b8485fb3146ae7cb9b7ebc9ac850
ThomasAnkcorn <thomasankcorn@gmail.com>
2016-09-30 10:09:24 +0100

    sgAddress updated more

--- projectx/src/main/java/projectx/entities/Address.java
+++ projectx/src/main/java/projectx/entities/Address.java
@@ -5,7 +5,9 @@ import javax.persistence.*;
 @NamedQueries({ @NamedQuery(name = Address.FIND_BY_ID, query = "Select a FROM Address a WHERE a.id= : Aid"),
 		@NamedQuery(name = Address.FIND_BY_LINE1, query = "Select a FROM Address a WHERE a.line_1= : line_1"),
 		@NamedQuery(name = Address.FIND_BY_LINE2, query = "Select a FROM Address a WHERE a.line_2= : line_2"),
-		@NamedQuery(name = Address.FIND_BY_POSTCODE, query = "Select a FROM Address a WHERE a.postcode= : postcode"), })
+		@NamedQuery(name = Address.FIND_BY_POSTCODE, query = "Select a FROM Address a WHERE a.postcode= : postcode"),
+		@NamedQuery(name = Address.INSERT_NEW_ADDRESS, query = "INSERT INTO Address (id, line_1, line_2, postcode) VALUES( :Aid, :line_1, :line_2, :postcode, :active)"),
+		@NamedQuery(name = Address.UPDATE_ACTIVE, query =  "UDATE Address  a SET a.active = :status WHERE a.id = :Aid")})
 @Entity
 @Table(name = "Address")
 public class Address {
@@ -14,6 +16,8 @@ public class Address {
 	public static final String FIND_BY_LINE1 = "Address.findByLine1";
 	public static final String FIND_BY_LINE2 = "Address.findByLine2";
 	public static final String FIND_BY_POSTCODE = "Address.findByPostCode";
+	public static final String INSERT_NEW_ADDRESS = "Address.new";
+	public static final String UPDATE_ACTIVE = "Address.deactivate";
 	@Id
 	@Column(name = "id", nullable = false)
 	private int id;
