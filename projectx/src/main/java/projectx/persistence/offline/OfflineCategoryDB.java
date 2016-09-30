f59af1f3b3527094ff7555306925b42007cca781
DanielH <dan.hopkins42@gmail.com>
2016-09-30 15:01:08 +0100

    changed how the data is laid out in the offline DB

---
+++ projectx/src/main/java/projectx/persistence/offline/OfflineCategoryDB.java
@@ -0,0 +1,16 @@
+package projectx.persistence.offline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Singleton;
+
+import projectx.persistence.entities.PaymentDetails;
+import projectx.persistence.entities.User;
+import projectx.persistence.entities.UserLevel;
+
+@Singleton
+public class OfflineCategoryDB 
+{
+	
+}
