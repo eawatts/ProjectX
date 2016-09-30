754a1babe229586c4fcdc86bbedc078fe6026d93
eawatts <ewattsie@hotmail.com>
2016-09-30 09:39:00 +0000

    Updated a few named queries in review entity

--- projectx/src/main/java/projectx/assets/Review.java
+++ projectx/src/main/java/projectx/assets/Review.java
@@ -7,79 +7,97 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
+
+@NamedQueries({
+	@NamedQuery(name = Review.VIEW_ALL,
+				query = "SELECT r FROM Review r"),
+	@NamedQuery(name = Review.VEIW_USER_ID,
+				query = "SELECT r.user_id FROM Review r"), 
+	@NamedQuery(name = Review.FIND_BY_USER_ID,
+				query = "SELECT r.user_id FROM Review r WHERE r.user_id = :user_id"),
+	})
+
 @Table(name = "Review")
 public class Review {
-		
-		@Id
-		@Column(name = "id", nullable = false)
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		
-		@Column(name = "rating", nullable = false)
-		@NotNull
-		private Long rating;
-		
-		@Column(name = "comment", length = 512)
-		@Size (min = 2, max = 512)
-		private String comment;	
-		
-		@ManyToOne
-		@JoinColumn(name = "user_id", nullable = false)
-		@NotNull
-		private User reviewer;
-		
-		@ManyToOne
-		@JoinColumn(name = "product_id", nullable = false)
-		@NotNull
-		private Product product;
-		
-		/**
-		 * Review constructor
-		 * 
-		 * @param id
-		 * @param rating
-		 * @param comment
-		 * @param reviewer
-		 * @param product
-		 */
-		public Review(Long id, Long rating, String comment, User reviewer, Product product) {
-			super();
-			this.id = id;
-			this.rating = rating;
-			this.comment = comment;
-			this.reviewer = reviewer;
-			this.product = product;
-		}
-		
-		public Long getId() {
-			return id;
-		}
-		public Long getRating() {
-			return rating;
-		}
-		public void setRating(Long rating) {
-			this.rating = rating;
-		}
-		public String getComment() {
-			return comment;
-		}
-		public void setComment(String comment) {
-			this.comment = comment;
-		}
-		public User getReviewer() {
-			return reviewer;
-		}
-		public Product getProduct() {
-			return product;
-		}
-		
-		
-//		@NamedQueries ({
-//			@NamedQuery(name = "", query = "")
-//		})
+	
+	public static final String VIEW_ALL = "Review.viewAll";
+	public static final String VEIW_USER_ID = "Review.viewUserId";
+	public static final String FIND_BY_USER_ID = "Review.findByUserId";
+
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "rating", nullable = false)
+	@NotNull
+	private Long rating;
+
+	@Column(name = "comment", length = 512)
+	@Size(min = 2, max = 512)
+	private String comment;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	@NotNull
+	private User reviewer;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id", nullable = false)
+	@NotNull
+	private Product product;
+
+	/**
+	 * Review constructor
+	 * 
+	 * @param id
+	 * @param rating
+	 * @param comment
+	 * @param reviewer
+	 * @param product
+	 */
+	public Review(Long id, Long rating, String comment, User reviewer, Product product) {
+		super();
+		this.id = id;
+		this.rating = rating;
+		this.comment = comment;
+		this.reviewer = reviewer;
+		this.product = product;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getRating() {
+		return rating;
+	}
+
+	public void setRating(Long rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public User getReviewer() {
+		return reviewer;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
 }
