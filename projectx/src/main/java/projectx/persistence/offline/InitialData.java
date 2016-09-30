17bc08f8c773a2d4583b57d2cb6260910d640f38
aboyce <adam@aboyce.uk>
2016-09-30 14:10:24 +0100

    *Added structure for InitialData.

--- projectx/src/main/java/projectx/persistence/offline/InitialData.java
+++ projectx/src/main/java/projectx/persistence/offline/InitialData.java
@@ -1,6 +1,35 @@
 package projectx.persistence.offline;
 
-public class InitialData {
-	// TODO: Populate this!
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.Singleton;
+
+import projectx.persistence.entities.User;
+
+@Singleton
+public class InitialData {
+	
+	private List<User> users;
+
+	public InitialData() {
+		populateData();
+	}
+	
+	private void populateData() {
+		users = new ArrayList<User>();
+		users.add(new User());
+	}
+	
+	public List<User> getUsers(){
+		return users;
+	}
+	
+	public void addUser(User user){
+		this.users.add(user);
+	}
+	
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
 }
