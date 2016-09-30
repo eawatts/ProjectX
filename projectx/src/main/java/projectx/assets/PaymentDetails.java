59aeb3911b6bab2779d6b11a4c33414c3a492ef2
kiranNeal <kiran.n.kulkarni@outlook.com>
2016-09-30 10:26:03 +0100

    added stuff to payment details

--- projectx/src/main/java/projectx/assets/PaymentDetails.java
+++ projectx/src/main/java/projectx/assets/PaymentDetails.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.Size;
 @NamedQueries ({
 	@NamedQuery (name=PaymentDetails.FIND_BY_USER, query= "SELECT d FROM PaymentDetails d WHERE d.user_id=:user_id"),
 	@NamedQuery (name=PaymentDetails.FIND_BY_ID, query= "SELECT d FROM PaymentDetails d WHERE d.id=:id"),
-	
+	@NamedQuery (name=PaymentDetails.CREATE_PAYMENTDETAIL, query= "INSERT into PaymentDetails (id, user_id, account_number, sort_code) VALUES (:id, :user_id, :account_number, :sort_code)"),
+	@NamedQuery (name=PaymentDetails.UPDATE_PAYMENTDETAIL, query= "UPDATE PaymentDetails SET (user_id=:user_id, account_number=:account_number, sort_code=:sort_code) WHERE id=:id"), 
+	@NamedQuery (name=PaymentDetails.SHOW_ALL, query= "SELECT * FROM PaymentDetails"),
 })
 
 @Entity
@@ -15,10 +17,9 @@ import javax.validation.constraints.Size;
 public class PaymentDetails {
 
 	@Id
-	@Column(name = "id", length=80, nullable=false)
+	@Column(name = "id", nullable=false)
 	@GeneratedValue (strategy= GenerationType.IDENTITY)
 	@NotNull
-	@Size(min=1, max=8)
 	private int id;
 	
 	@ManyToOne
@@ -118,5 +119,8 @@ public class PaymentDetails {
 	}
 	public static final String FIND_BY_USER= "PaymentDetails.findByUser";
 	public static final String FIND_BY_ID= "PaymentDetails.findById";
+	public static final String CREATE_PAYMENTDETAIL= "PaymentDetails.createPaymentDetail";
+	public static final String UPDATE_PAYMENTDETAIL= "PaymentDetails.UpdatePaymentDetail";
+	public static final String SHOW_ALL= "PaymentDetails.showAll";
 
 }
---
+++ projectx/src/main/java/repository_interfaces/PaymentDetailsRepository.java
@@ -0,0 +1,5 @@
+package repository_interfaces;
+
+public class PaymentDetailsRepository {
+
+}
