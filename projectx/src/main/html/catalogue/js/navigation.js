396e51309b2c05299c82506b2eff7544d52bf8f1
aboyce <adam@aboyce.uk>
2016-10-11 11:55:37 +0100

    *Progress on Template.

--- projectx/src/main/html/catalogue/js/navigation.js
+++ projectx/src/main/html/catalogue/js/navigation.js
@@ -21,3 +21,24 @@ function displayNavigation() {
     $("#nav-desktop").show();
   }
 }
+
+// 
+// jQuery(function($){
+//   $( '.menu-btn' ).click(function(){
+//     $('.responsive-menu').addClass('expand')
+//     $('.menu-btn').addClass('btn-none')
+//   })
+//
+//   $( '.close-btn' ).click(function(){
+//     $('.responsive-menu').removeClass('expand')
+//     $('.menu-btn').removeClass('btn-none')
+//   })
+// })
+//
+//
+
+jQuery(function($){
+  $( '.menu-btn' ).click(function(){
+    $('.responsive-menu').toggleClass('expand')
+  })
+})
