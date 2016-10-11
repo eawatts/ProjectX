735d2e69e22a72af2400089d78f28f4d16cd1bc1
aboyce <adam@aboyce.uk>
2016-10-11 11:29:30 +0100

    *Added FontAwesome. *Added JQuery. *Added functionality to template to remove the desktop navigation when the size gets too small. *General template improvements.

---
+++ projectx/src/main/html/catalogue/js/navigation.js
@@ -0,0 +1,23 @@
+
+// When the page is loaded, check which navigation to show.
+$(document).ready(function() {
+  displayNavigation();
+});
+
+// Whenever the window screen size changes, check which navigation to show.
+$(window).resize(function() {
+  displayNavigation();
+});
+
+
+// Will get the width of the window, and see if it is less that
+//  600px (Mobile Size), and will swap the navigation as fit.
+function displayNavigation() {
+  if ($(window).width() <= 600){
+    $("#nav-mobile").show();
+    $("#nav-desktop").hide();
+  } else {
+    $("#nav-mobile").hide();
+    $("#nav-desktop").show();
+  }
+}
