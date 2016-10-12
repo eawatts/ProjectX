a7442ee195dc126d507acb4044495b025d2e4834
aboyce <adam@aboyce.uk>
2016-10-12 16:29:27 +0100

    *Vast improvements on the mobile view. The navigation is now full screen, animated and indicates that it is selected.

--- projectx/src/main/html/catalogue/js/header.js
+++ projectx/src/main/html/catalogue/js/header.js
@@ -1,37 +1,30 @@
-
 // When the page is loaded, check which navigation to show.
 $(document).ready(function() {
-  displayNavigation();
+  displayHeader();
 });
 
 // Whenever the window screen size changes, check which navigation to show.
 $(window).resize(function() {
-  displayNavigation();
+  displayHeader();
 });
 
 // Will get the width of the window, and see if it is less that
 //  600px (Mobile Size), and will swap the navigation as fit.
-function displayNavigation() {
+function displayHeader() {
   if ($(window).width() <= 992){
-    $("#nav-mobile").show();
     $("#nav-desktop").hide();
+    $("#header-mobile").show();
+    $("#header-desktop").hide();
   } else {
-    $("#nav-mobile").hide();
     $("#nav-desktop").show();
+    $("#header-mobile").hide();
+    $("#header-desktop").show();
   }
 }
 
 jQuery(function($){
   $(".mobile-nav-toggle").click(function(){
-    $(".mobile-nav-menu").toggleClass("mobile-nav-menu-expand")
+    $(".button-burger-nav").toggleClass("button-burger-nav-selected");
+    $(".mobile-nav-menu").slideToggle("slow", null);
   })
-})
-
-jQuery(function($){
-  $(".mobile-nav-toggle-burger").hover(function() {
-    $(".mobile-nav-toggle-burger").css("background", "#a53f2b");
-    $(".mobile-nav-toggle-burger").css("cursor", "pointer");
-  }, function() {
-    $(".mobile-nav-toggle-burger").css("background", "#f9fbf2");
-  });
 })
