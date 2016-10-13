e8335fb3fcb2e07256e61cf168ad4b49a73eee17
DanielH <dan.hopkins42@gmail.com>
2016-10-13 12:52:43 +0100

    initial restructuring into catalogue and ims as separate systems

---
+++ catalogue/src/main/java/projectx/persistence/repositories/interfaces/CategoryRepository.java
@@ -0,0 +1,22 @@
+package projectx.persistence.repositories.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+
+import projectx.persistence.entities.Category;
+
+@Stateless
+@Default
+public interface CategoryRepository
+{
+	
+	public void persistCategory(Category category);
+	public void persistCategories(List<Category> categories);
+	public List<Category> getCategories();
+	public void updateCategory(Category category);
+	public Category findByName(String name);
+	public Category findByid(long id);
+	
+}
