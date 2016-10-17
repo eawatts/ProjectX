c3c368392f1196f38cd2275a48884cace9b751fa
kiranNeal <kiran.n.kulkarni@outlook.com>
2016-10-17 15:02:12 +0100

    Login COntroller

--- ims/src/main/java/projectx/persistence/controllers/LoginController.java
+++ ims/src/main/java/projectx/persistence/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package projectx.persistence.controllers;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named(value="login")
@@ -13,8 +15,38 @@ public class LoginController {
 	private String password = "";
 	private String error = "";
 	
-	
-	
-
+	public String login(){
+		if (username.equals("")){
+		error = "please enter a username";
+		password = "";
+		return "login";
+		}
+		if (password.equals("")) {
+		error = "please enter a password";
+		password = "";
+		return "login";
+		}
+		if(!customerService.validateDetails(username, password)) {
+				error = "Invalid login";
+				password = "";
+				return "login";
+				}
+				userCredentials.setUser(loginService.getUserID(username));
+				return "home";
+	}
+	public String logout() {
+		userCredentials.setUser("");
+		return "login";
+		}
+		public String getUsername(){
+		return username; }
+		public String getPassword(){
+		return password; }
+		public String getError(){
+		return error; }
+		public void setUsername(String user) {
+		this.username = user; }
+		public void setPassword(String pass){
+		this.password = pass; }
 	
 }
