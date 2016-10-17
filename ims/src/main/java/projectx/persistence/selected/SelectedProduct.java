91b6434b9c143c03b89efca1b9651259977420ce
DanielH <dan.hopkins42@gmail.com>
2016-10-17 15:10:08 +0100

    finished selected products and Product controller

--- ims/src/main/java/projectx/persistence/selected/SelectedProduct.java
+++ ims/src/main/java/projectx/persistence/selected/SelectedProduct.java
@@ -1,6 +1,33 @@
 package projectx.persistence.selected;
 
-public class SelectedProduct
-{
+import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import projectx.persistence.entities.Product;
+import projectx.persistence.services.ProductService;
+@SessionScoped
+@Named("selected")
+public class SelectedProduct implements Serializable
+{
+	@Inject private ProductService proServ;
+	private Product selectedProduct;
+	/**
+	 * @return the selectedProduct
+	 */
+	public Product getSelectedProduct()
+	{
+		return selectedProduct;
+	}
+	/**
+	 * @param selectedProduct the selectedProduct to set
+	 */
+	public void setSelectedProduct(Product selectedProduct)
+	{
+		this.selectedProduct = selectedProduct;
+	}
+	
+	
 }
