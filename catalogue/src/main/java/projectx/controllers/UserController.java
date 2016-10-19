1b4e8e39ea0d83287bdeed7f6d6df4f452c304de
ThomasAnkcorn <thomasankcorn@gmail.com>
2016-10-19 15:11:35 +0100

    stubs for product controller

--- catalogue/src/main/java/projectx/controllers/UserController.java
+++ catalogue/src/main/java/projectx/controllers/UserController.java
@@ -6,6 +6,8 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import projectx.services.AddressService;
+import projectx.services.BankAccountService;
 import projectx.services.UserService;
 
 @Named("products")
@@ -19,6 +21,31 @@ public class UserController implements Serializable{
 	
 	@Inject
 	private UserService userService;
+	private AddressService addressService;
+	private BankAccountService bankService;
 	
+	private String title;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String bio;
+	private int housenumber;
+	private String street;
+	private String town;
+	private char[] postcode;
+	private int[] banknumber;
+	private int[] banksort;
+
+
+	public void submit(){
+		userService.title=title;
+		userService.email=email;
+		userService.firstname=firstName;
+		userService.lastName=lastName;
+		userService.age=age;
+		userService.bio=bio;
+		addressService.housenumber=housenumber
+	}
 	
 }
