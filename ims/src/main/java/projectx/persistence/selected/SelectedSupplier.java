364fdf3d4fe360609575d3632501aa88e9590b5d
kiranNeal <kiran.n.kulkarni@outlook.com>
2016-10-19 14:40:13 +0100

    changing Supplier controller

--- ims/src/main/java/projectx/persistence/selected/SelectedSupplier.java
+++ ims/src/main/java/projectx/persistence/selected/SelectedSupplier.java
@@ -14,27 +14,13 @@ public class SelectedSupplier implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -1810508961661962235L;
-	@Inject 
-	private PurchaseOrderRepository purchaseOrder;
-	@Inject
-	private SupplierService supplierService;
+	private static final long serialVersionUID = 1L;
 	private Supplier supplier;
 	
-	public PurchaseOrderRepository getPurchaseOrder(){
-		return purchaseOrder;
-	}
-	
-	public void setSupplier(PurchaseOrderRepository purchaseOrder){
-		this.purchaseOrder=purchaseOrder;
-		supplier = supplierService.findById(purchaseOrder.getSupplier());
-	}
-
-	public Supplier getSupplier() {
+	public Supplier getSupplier(){
 		return supplier;
 	}
-
-	public void setSupplier(Supplier supplier) {
-		this.supplier = supplier;
+	public void setSupplier(Supplier supplier){
+		this.supplier=supplier;
 	}
 }
