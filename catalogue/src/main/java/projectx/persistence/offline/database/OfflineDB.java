65422dc152644d5b7051e8718538c3f25775ef84
eawatts <ewattsie@hotmail.com>
2016-10-20 12:02:15 +0000

    modified product entity

--- catalogue/src/main/java/projectx/persistence/offline/database/OfflineDB.java
+++ catalogue/src/main/java/projectx/persistence/offline/database/OfflineDB.java
@@ -41,21 +41,19 @@ public class OfflineDB {
 
 	private void setupProducts() {
 		products = new ArrayList<Product>();
-		products.add(new Product(1, "Red Gnome 1", "This gnome is lovely", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(2, "Red Gnome 2", "Very generic gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(3, "Red Gnome 3", "Bogstandard", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(
-				new Product(4, "Red Gnome 4", "This is a stubbstandard gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(5, "Red Gnome 5", "Red-hatted gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(6, "Red Gnome 6", "Classic Gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(7, "Red Gnome 7", "Usual red gnome with hat", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(8, "Red Gnome 8", "Classic red hat gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(9, "Red Gnome 9", "The gnome of gnomes", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(10, "Red Gnome 10", "The one true gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(11, "Red Gnome 11", "Large red hat gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(new Product(12, "Red Gnome 12", "Your dream gnome", 100, new BigDecimal(19.99), 10, 100, 1));
-		products.add(
-				new Product(13, "Red Gnome 13", "Classic gnome - medium size", 100, new BigDecimal(19.99), 10, 100, 1));
+		products.add(new Product(1, "Red Gnome 1", "This gnome is lovely", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(2, "Red Gnome 2", "Very generic gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(3, "Red Gnome 3", "Bogstandard", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(4, "Red Gnome 4", "This is a stubbstandard gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/img/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(5, "Red Gnome 5", "Red-hatted gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(6, "Red Gnome 6", "Classic Gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(7, "Red Gnome 7", "Usual red gnome with hat", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(8, "Red Gnome 8", "Classic red hat gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(9, "Red Gnome 9", "The gnome of gnomes", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(10, "Red Gnome 10", "The one true gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(11, "Red Gnome 11", "Large red hat gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(12, "Red Gnome 12", "Your dream gnome", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
+		products.add(new Product(13, "Red Gnome 13", "Classic gnome - medium size", 100, new BigDecimal(19.99), 10, 100, 1, "/products/gnomes/gnome_red-hat.png"));
 
 	}
 
@@ -64,19 +62,23 @@ public class OfflineDB {
 		users.add(new User(1, "JSmithy", "password", "John", "Smith", UserLevel.ADMIN, "john.smith@email.com"));
 		users.add(new User(2, "Bazza", "password", "Barry", "Smith", UserLevel.CUSTOMER, "barry.smith@email.com"));
 		users.add(new User(3, "JaneMane", "password", "Jane", "Smith", UserLevel.CUSTOMER, "jane.smith@email.com"));
-		users.add(
-				new User(4, "Sallysaurus", "password", "Sally", "Smith", UserLevel.CUSTOMER, "sally.smith@email.com"));
+		users.add(new User(4, "Sallysaurus", "password", "Sally", "Smith", UserLevel.CUSTOMER, "sally.smith@email.com"));
 		users.add(new User(5, "Stevie", "password", "Steve", "Smith", UserLevel.CUSTOMER, "steve.smith@email.com"));
+		users.add(new User(6, "GroovyMan", "password", "Harvey", "Stevens", UserLevel.CUSTOMER, "harvey.stevens@email.com"));
+		users.add(new User(7, "Spud", "password", "Samuel", "Potts", UserLevel.CUSTOMER, "samuel.potts@email.com"));
+		users.add(new User(8, "Stubb", "password", "Brian", "McCafe", UserLevel.CUSTOMER, "brian.mccafe@email.com"));
+		users.add(new User(9, "Freddo", "password", "Fred", "O'Donnell", UserLevel.CUSTOMER, "fred.odonnell@email.com"));
+		users.add(new User(10, "Freddeta", "password", "Frida", "O'Donnell", UserLevel.CUSTOMER, "frida.odonnell@email.com"));
 	}
 
 	private void setupReviews() {
 		reviews = new ArrayList<Review>();
 		reviews.add(new Review(1, 4.5f, "Pretty dank.", users.get(2), products.get(2)));
-		reviews.add(new Review(2, 3.1f, "Pretty good.", users.get(3), products.get(3)));
+		reviews.add(new Review(2, 3.1f, "Pretty good.", users.get(7), products.get(3)));
 		reviews.add(new Review(3, 2.0f, "Broke first night.", users.get(2), products.get(4)));
 		reviews.add(new Review(4, 4.9f, "Pretty good.", users.get(4), products.get(8)));
 		reviews.add(new Review(5, 5f, "Best. Purchase Ever!.", users.get(2), products.get(8)));
-		reviews.add(new Review(6, 3.8f, "Pretty good.", users.get(3), products.get(8)));
+		reviews.add(new Review(6, 3.8f, "Pretty good.", users.get(6), products.get(8)));
 		reviews.add(new Review(7, 4.2f, "Worth the money..", users.get(2), products.get(1)));
 		reviews.add(new Review(8, 4.2f, "Outstanding.", users.get(3), products.get(1)));
 		reviews.add(new Review(9, 3f, "Pretty good.", users.get(4), products.get(1)));
@@ -85,10 +87,10 @@ public class OfflineDB {
 		reviews.add(new Review(12, 5f, "My little Harry loves it.", users.get(4), products.get(1)));
 		reviews.add(new Review(13, 3.8f, "Looks great next to the pond.", users.get(3), products.get(1)));
 		reviews.add(new Review(14, 3.5f, "Pros: Price. Cons: Quality", users.get(3), products.get(1)));
-		reviews.add(new Review(15, 3.9f, "Its my second one!", users.get(3), products.get(1)));
+		reviews.add(new Review(15, 3.9f, "Its my second one!", users.get(6), products.get(1)));
 		reviews.add(new Review(16, 4.2f, "Absolute scorcher.", users.get(3), products.get(1)));
-		reviews.add(new Review(17, 4.6f, "Perfect proportions.", users.get(3), products.get(1)));
-		reviews.add(new Review(18, 0.5f, "Tripping hazard.", users.get(3), products.get(1)));
+		reviews.add(new Review(17, 4.6f, "Perfect proportions.", users.get(8), products.get(1)));
+		reviews.add(new Review(18, 0.5f, "Tripping hazard. Lorem ipsum dolor sit amet, eu sit dignissim conceptam, in dicta nullam vim, indoctum instructior eu quo. Duo ex vocent iuvaret vituperatoribus, nam ludus nostro eruditi an. Eam legere alterum cu. Id solum rebum apeirian cum, sed in aliquando elaboraret. Te qui vero dicant probatus.", users.get(9), products.get(1)));
 	}
 
 	private void setupCategories() {
