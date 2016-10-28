9aaebf48e52d7527fcd7bcec3796e17e9a0fd26e
aboyce <adam@aboyce.uk>
2016-10-28 09:13:16 +0100

    *More refactoring.

--- catalogue/src/main/java/projectx/persistence/offline/CategoryRepositoryOffline.java
+++ catalogue/src/main/java/projectx/persistence/offline/CategoryRepositoryOffline.java
@@ -2,51 +2,60 @@ package projectx.persistence.offline;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
-import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
 import projectx.persistence.entities.Category;
-import projectx.persistence.offline.database.OfflineDB;
 import projectx.persistence.repositories.interfaces.CategoryRepository;
 
-@Stateless
-@Default
 public class CategoryRepositoryOffline implements CategoryRepository {
 	
 	@Inject
-	private OfflineDB db;
+	private InitialData db;
 
 	@Override
-	public void persistCategory(Category category) {
-		// TODO Auto-generated method stub
+	public void persistCategory(Category category)
+	{
+		db.addCategory(category);
 		
 	}
 
 	@Override
-	public void persistCategories(List<Category> categories) {
-		// TODO Auto-generated method stub
+	public void persistCategories(List<Category> categories)
+	{
+		for(Category category : categories)
+		{
+			persistCategory(category);
+		}
 		
 	}
 
 	@Override
-	public List<Category> getCategories() {
+	public List<Category> getCategories()
+	{
 		return db.getCategories();
 	}
 
 	@Override
-	public void updateCategory(Category category) {
+	public void updateCategory(Category category)
+	{
 		// TODO Auto-generated method stub
+		db.updateCategory(category);
 		
 	}
 
 	@Override
-	public Category findByName(String name) {
-		return db.findByName(name);
+	public Category findByName(String name)
+	{
+		// TODO Auto-generated method stub
+		return db.findCategoryByName(name);
 	}
 
-
-	public Category findById(int id) {
-		return db.findById(id);
+	@Override
+	public Category findByid(long id)
+	{
+		// TODO Auto-generated method stub
+		return db.findCategoryById(id);
 	}
+
+	
 }
