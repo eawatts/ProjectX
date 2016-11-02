3f7ea26d9ac2e629b87b1e2f25de10338868e222
eawatts <ewattsie@hotmail.com>
2016-11-02 10:13:26 +0000

    testring eds code

---
+++ ims/src/main/webapp/js/po_expand.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	alert("test message");
+//    $("#hide").click(function(){
+//        $("p").hide();
+//    });
+//    $("#show").click(function(){
+//        $("p").show();
+//    });
+});
+alert("test message");
