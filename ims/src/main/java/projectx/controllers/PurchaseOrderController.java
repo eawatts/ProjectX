a85939a41099cfa42445ccbfa6ccf1e237dec55f
aboyce <adam@aboyce.uk>
2016-11-08 15:56:22 +0100

    *Formatting.

--- ims/src/main/java/projectx/controllers/PurchaseOrderController.java
+++ ims/src/main/java/projectx/controllers/PurchaseOrderController.java
@@ -1,6 +1,7 @@
 package projectx.controllers;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -10,28 +11,38 @@ import projectx.persistence.entities.Product;
 import projectx.persistence.entities.Supplier;
 import projectx.persistence.selected.SelectedPurchaseOrderProduct;
 import projectx.persistence.webentities.CurrentSession;
+import projectx.persistence.webentities.PurchaseOrderProduct;
 
 @Named("purchase_order")
 @RequestScoped
-public class PurchaseOrderController implements Serializable{
-	
+public class PurchaseOrderController implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private CurrentSession currentsession;
-	
+
 	@Inject
 	private SelectedPurchaseOrderProduct selectedPurchaseOrderProduct;
 
-	
 	public String addPendingPurchaseOrder() {
-		
+
 		Product selectedProduct = selectedPurchaseOrderProduct.getProduct();
 		int selectedQuantity = selectedPurchaseOrderProduct.getQuantity();
+		
+		List<PurchaseOrderProduct> purchaseOrderProducts = currentsession.getPendingPurchaseOrder().getContents();
+
+		// See if we need to update the quantity or add a new entry
+		for (PurchaseOrderProduct purchaseOrderProduct : purchaseOrderProducts) {
+			if (purchaseOrderProduct.getProduct().getId() == selectedProduct.getId()) {
+				purchaseOrderProduct.setQuantity(purchaseOrderProduct.getQuantity() + selectedQuantity);
+				return null;
+			}
+		}
+
 		Supplier selectedSupplier = selectedPurchaseOrderProduct.getSupplier();
-		
 		currentsession.getPendingPurchaseOrder().addProductToPurchaseOrder(selectedProduct, selectedQuantity, selectedSupplier);
-		
+
 		return null;
 	}
 }
