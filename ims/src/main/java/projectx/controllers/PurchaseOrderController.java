7d4ff1967ec29ee02c5061cb58458cf9f7b89566
aboyce <adam@aboyce.uk>
2016-11-09 15:11:56 +0100

    *Added logic to roughly generate a Purchase Order.

--- ims/src/main/java/projectx/controllers/PurchaseOrderController.java
+++ ims/src/main/java/projectx/controllers/PurchaseOrderController.java
@@ -1,5 +1,5 @@
 package projectx.controllers;
-		
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +13,7 @@ import projectx.persistence.entities.Supplier;
 import projectx.persistence.selected.SelectedPurchaseOrderProduct;
 import projectx.persistence.webentities.CurrentSession;
 import projectx.persistence.webentities.PurchaseOrderProduct;
+import projectx.services.ProductService;
 
 @Named("purchase_order")
 @RequestScoped
@@ -24,6 +25,9 @@ public class PurchaseOrderController implements Serializable {
 	private CurrentSession currentsession;
 
 	@Inject
+	private ProductService productServce;
+
+	@Inject
 	private SelectedPurchaseOrderProduct selectedPurchaseOrderProduct;
 
 	public String addPendingPurchaseOrderProduct() {
@@ -31,45 +35,61 @@ public class PurchaseOrderController implements Serializable {
 		Product selectedProduct = selectedPurchaseOrderProduct.getProduct();
 		int selectedQuantity = selectedPurchaseOrderProduct.getQuantity();
 		Supplier selectedSupplier = selectedPurchaseOrderProduct.getSupplier();
-		
+
 		List<PurchaseOrderProduct> purchaseOrderProducts = currentsession.getPendingPurchaseOrder().getContents();
 
-		// See if we just need to update the quantity if it is exactly the same product.
+		// See if we just need to update the quantity if it is the same product.
 		for (PurchaseOrderProduct purchaseOrderProduct : purchaseOrderProducts) {
-			if ((purchaseOrderProduct.getProduct().getId() == selectedProduct.getId()) && (purchaseOrderProduct.getSupplier().getId() == selectedSupplier.getId())) {
+			if ((purchaseOrderProduct.getProduct().getId() == selectedProduct.getId())
+					&& (purchaseOrderProduct.getSupplier().getId() == selectedSupplier.getId())) {
 				purchaseOrderProduct.setQuantity(purchaseOrderProduct.getQuantity() + selectedQuantity);
 				return null;
 			}
 		}
 
-		currentsession.getPendingPurchaseOrder().addProductToPurchaseOrder(selectedProduct, selectedQuantity, selectedSupplier);
+		currentsession.getPendingPurchaseOrder().addProductToPurchaseOrder(selectedProduct, selectedQuantity,
+				selectedSupplier);
 
 		return null;
 	}
-	
+
 	public String clearAllPendingPurchaseOrderProducts() {
-		currentsession.getPendingPurchaseOrder().setPurchaseOrderContents(new ArrayList<PurchaseOrderProduct>());		
+		currentsession.getPendingPurchaseOrder().setPurchaseOrderContents(new ArrayList<PurchaseOrderProduct>());
 		return null;
 	}
-	
+
 	public String removePendingPurchaseOrderProduct(int productId, int supplierId) {
-		
+
 		List<PurchaseOrderProduct> purchaseOrderProducts = currentsession.getPendingPurchaseOrder().getContents();
 		for (PurchaseOrderProduct purchaseOrderProduct : purchaseOrderProducts) {
-			if ((purchaseOrderProduct.getProduct().getId() == productId) && (purchaseOrderProduct.getSupplier().getId() == supplierId)) {
+			if ((purchaseOrderProduct.getProduct().getId() == productId)
+					&& (purchaseOrderProduct.getSupplier().getId() == supplierId)) {
 				purchaseOrderProducts.remove(purchaseOrderProduct);
 				return null;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
+	public String generatePurchaseOrder() {
+
+		// Get the standard Products
+		List<Product> lowStockProducts = productServce.getTop25LowStockProducts();
+
+		List<PurchaseOrderProduct> purchaseOrderProducts = currentsession.getPendingPurchaseOrder().getContents();
+
+		for (Product product : lowStockProducts) {
+			purchaseOrderProducts.add(new PurchaseOrderProduct(product, 5, new Supplier()));
+		}
+
+		currentsession.getPendingPurchaseOrder().setPurchaseOrderContents(purchaseOrderProducts);
+		return null;
+	}
+
 	public String completePurchaseOrder() {
-		
-		//List<PurchaseOrderProduct> purchaseOrderProducts = currentsession.getPendingPurchaseOrder().getContents();
-		
-		
+		// List<PurchaseOrderProduct> purchaseOrderProducts =
+		// currentsession.getPendingPurchaseOrder().getContents();
 		clearAllPendingPurchaseOrderProducts();
 		return "index";
 	}
